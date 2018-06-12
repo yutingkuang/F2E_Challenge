4d86266cbe62ad1dd6e5f1a5a38721c73dd36c34
ytk <yutingkuang@gmail.com>
2018-06-12 09:33:08 +0800

    ?

--- app/dist/core/container/_assets.js
+++ app/dist/core/container/_assets.js
@@ -1,5 +1,7 @@
 export default [
   require('@/css/bootstrap.min.css'),
   require('@/css/font-awesome.min.css'),
+  require('@/css/res-media.scss'),
+  require('@/css/responsive.scss'),
   require('@/css/shared.css')
 ];
