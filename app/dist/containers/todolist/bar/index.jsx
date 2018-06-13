9d2b4f9ddbacf7af59d8911960f2c7ef2bdaa7b6
ytk <yutingkuang@gmail.com>
2018-06-13 12:23:15 +0800

    上面那一條切版

--- app/dist/containers/todolist/bar/index.jsx
+++ app/dist/containers/todolist/bar/index.jsx
@@ -4,9 +4,13 @@ import { withStyle } from '~/core/container';
 import styles from './bar.scss';
 
 const View = ({}) => (
-  <div styleName="navbar navbar-expand navbar-dark">
-    <div />
+  <div styleName="bar">
+    <div styleName="nav-bar">
+      <div styleName="nav active">My Tasks</div>
+      <div styleName="nav">In Progress</div>
+      <div styleName="nav">Completed</div>
+    </div>
   </div>
 );
 
-export default withStyle()(View);
+export default withStyle(styles)(View);
