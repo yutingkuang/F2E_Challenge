8043a020653c019fd51339f08c3572498be28b37
ytk <yutingkuang@gmail.com>
2018-06-14 10:22:08 +0800

    把todoform拆出來獨立元件 改寫additem元件

--- app/dist/containers/todolist/additem/handler.js
+++ app/dist/containers/todolist/additem/handler.js
@@ -1,68 +1,12 @@
 //@flow
-import { emit } from '~/core/action/effects';
-import { compose, withStyle, withDispatch } from '~/core/container';
-import { withState, withStateHandlers, withHandlers } from 'recompose';
-import { ADD_TODO } from '../reducer';
+import { compose, withStyle } from '~/core/container';
+import { withState, withHandlers } from 'recompose';
 import styles from './add.scss';
 
 export default compose(
-  withDispatch,
-  withStateHandlers(
-    {
-      isOpen: false,
-      todo: {
-        todo: '',
-        isDone: false,
-        isImportant: false,
-        attached: null,
-        due: null,
-        comment: null
-      }
-    },
-    {
-      setOpen: _ => isOpen => ({
-        isOpen
-      }),
-      setImportant: ({ todo }) => isImportant => ({
-        todo: { ...todo, isImportant }
-      }),
-      setAttached: ({ todo }) => attached => ({
-        todo: { ...todo, attached }
-      }),
-      setDone: ({ todo }) => isDone => ({
-        todo: { ...todo, isDone }
-      }),
-      setDue: ({ todo }) => due => ({
-        todo: { ...todo, due }
-      }),
-      setTodo: ({ todo: preTodo }) => todo => ({
-        todo: { ...preTodo, todo }
-      }),
-      changeInput: ({ todo: preTodo }) => ({ target: { value, name } }) => ({
-        todo: { ...preTodo, [name]: value }
-      }),
-      resetTodo: _ => _ => ({
-        todo: {
-          todo: '',
-          isDone: false,
-          isImportant: false,
-          attached: null,
-          due: null,
-          comment: null
-        }
-      })
-    }
-  ),
+  withState('isOpen', 'setOpen', false),
   withHandlers({
-    setOpen: ({ setOpen }) => isOpen => _ => setOpen(isOpen),
-    setImportant: ({ setImportant, todo: { isImportant } }) => _ =>
-      setImportant(!isImportant),
-    setDone: ({ setDone, todo: { isDone } }) => _ => setDone(!isDone),
-    addTodo: ({ todo, dispatch, setOpen, resetTodo }) => _ => {
-      dispatch(emit(ADD_TODO, todo));
-      resetTodo();
-      setOpen(false);
-    }
+    setOpen: ({ setOpen }) => isOpen => _ => setOpen(isOpen)
   }),
   withStyle(styles)
 );
